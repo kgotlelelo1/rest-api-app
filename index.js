2b7b03d86d32dc85a899c4ad19d0e57e5fc766b8
Kgotlelelo Mangene <Kgotlelelo.Mangene@bbd.co.za>
2025-01-20 14:50:00 +0200

    updated test script

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
 
 app.get("/msg", (req, res, next) => {
-    res.json({"message": "Hello, World!"});
-   });
+  res.json({ message: "Hello, World!" });
+});
 
-   app.use(express.json()); // for parsing application/json
+app.use(express.json());
 
-   app.post("/msg", (req, res, next) => {
-    const message = req.body.message;
-    res.json({"receivedMessage": message});
-   });
+app.post("/msg", (req, res, next) => {
+  const message = req.body.message;
+  res.json({ receivedMessage: message });
+});
